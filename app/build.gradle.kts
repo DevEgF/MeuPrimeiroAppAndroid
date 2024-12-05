59eddda32d8660e91f1b446c36f4749c65074ad1
Egito Filho <egitofilho4@gmail.com>
2024-12-05 20:46:52 -0300

    02_19_GERENCIANDO_DEPENDENCIAS_COM_O_CATALOGO_DE_VERSOES

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,12 +40,7 @@ android {
 
 dependencies {
 
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
+    implementation(libs.bundles.androidx)
     implementation(libs.material)
-    implementation(libs.androidx.activity)
-    implementation(libs.androidx.constraintlayout)
     testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
 }
